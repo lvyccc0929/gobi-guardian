dc7041a1ab81035fa784414e177d636ac1703103
Gobi Guardian <user@gobi-guardian.dev>
2026-07-02 13:14:46 +0800

    添加手机自动横屏显示功能

---
+++ landscape.js
@@ -0,0 +1,20 @@
+﻿(function () {
+  var styleEl = document.createElement("style");
+  styleEl.id = "landscape-force";
+  styleEl.textContent =
+    "@media screen and (orientation: portrait) and (max-width: 900px) {" +
+    "  html, body {" +
+    "    width: 100vh;" +
+    "    height: 100vw;" +
+    "  }" +
+    "  body {" +
+    "    transform: rotate(-90deg);" +
+    "    transform-origin: left top;" +
+    "    position: absolute;" +
+    "    top: 100%;" +
+    "    left: 0;" +
+    "    overflow: hidden;" +
+    "  }" +
+    "}";
+  document.head.appendChild(styleEl);
+})();
